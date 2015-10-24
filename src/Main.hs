b70b5768d418161ea75fd72677fa59760e98da88
Artem Tsushko <artem.tsushko@gmail.com>
2015-10-24 13:59:24 +0300

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Artem Tsushko, 2015
+-- License     :  BSD3
+--
+-- Maintainer  :  artem.tsushko@gmail.com
+-- Stability   :  stable
+-- Portability :  portable
+--
+--
+-----------------------------------------------------------------------------
+
+module Main (
+    main
+) where
+
+main :: IO ()
+main = putStrLn ""
